493dc27d27f1a671e25f1e1ece451e464ef4ea7b
my0brain0is0dead <118047676+my0brain0is0dead@users.noreply.github.com>
2023-08-07 14:07:01 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,16 @@
+module.exports = {
+    images: {
+        juggernaut:'https://e1.pxfuel.com/desktop-wallpaper/667/159/desktop-wallpaper-moa-games-juggernaut-dota-2-juggernaut-dota-2-mobile.jpg',
+        WR:'https://i.pinimg.com/originals/4e/07/86/4e07867de7e7d913d00514e0506168aa.png',
+        DR:'https://cdna.artstation.com/p/assets/images/images/044/268/756/large/boyang-zhu-drow-promo-updated-color-change.jpg?1639536571',
+        lina:'https://rare-gallery.com/livewalls/imgpreview/91757-arts-games-anime-girls-anime-graphics-lina-dota.webp',
+        TA:'https://images.hdqwalls.com/wallpapers/dota-2-templar-assassin-cosmetic-set-4k-mg.jpg',
+        VS:'https://steamuserimages-a.akamaihd.net/ugc/545280244440647891/F8FAC729651490ACE504A53384A079C900F7FED5/?imw=1024&imh=576&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true'
+    },
+
+    responses: {
+        start:'Добро пожаловать. /commands для просмотра доступных команд',
+        commands:'Доступные команды:\r\n/windranger\r\n/drow_ranger\r\n/lina\r\n/templar_assasin\r\n/vengeful_spirit\r\n/game',
+        guess_right:'Поздравляю, ты отгадал цифру!'
+    }
+}
